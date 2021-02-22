0804a0d79accc6a92913b17df8ab89204df6ab49
Nathan Bean <nhbean@ksu.edu>
2021-02-21 21:42:38 -0600

    Initial commit

---
+++ PhysicsExampleCGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PhysicsExampleCGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PhysicsExampleCGame())
+                game.Run();
+        }
+    }
+}
